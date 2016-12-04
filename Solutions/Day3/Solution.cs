d301a290c591bb2ff3098bf473e175fbe5eaa95b
Niels Keetlaer <niels@infi.nl>
2016-12-04 11:40:55 +0100

    Make Day 3 a teensy tiny bit more async

--- Solutions/Day3/Solution.cs
+++ Solutions/Day3/Solution.cs
@@ -9,16 +9,16 @@ namespace AoC2016.Solutions.Day3
             throw new System.NotImplementedException();
         }
 
-        public Task<string> SolveSilverAsync(string input)
+        public async Task<string> SolveSilverAsync(string input)
         {
-            var valid = new NormalWall(input).CountValidTriangles();
-            return Task.FromResult($"It seems that there are only {valid} valid triangles described on the wall");
+            var valid = await Task.Run(() => new NormalWall(input).CountValidTriangles());
+            return $"It seems that there are only {valid} valid triangles described on the wall";
         }
 
-        public Task<string> SolveGoldAsync(string input)
+        public async Task<string> SolveGoldAsync(string input)
         {
-            var valid = new IdioticWall(input).CountValidTriangles();
-            return Task.FromResult($"Apparently there are {valid} valid triangles described on the wall in a very odd way");
+            var valid = await Task.Run(() => new IdioticWall(input).CountValidTriangles());
+            return $"Apparently there are {valid} valid triangles described on the wall in a very odd way";
         }
     }
 }
