3bd28b9b208cb495b5c007570245ca00c94fd3b0
Niels Keetlaer <niels@keetlaer.net>
2016-12-18 17:41:53 +0100

    Simplify solution for Day 13 by using BFS instead of A* This should also make it more robust since A* is optimzed to balance between finding the best route and being the least cpu intensive

--- Solutions/Day13/PathFinder.cs
+++ Solutions/Day13/PathFinder.cs
@@ -17,36 +17,19 @@ namespace AoC2016.Solutions.Day13
             _maze = maze;
         }
 
-        // Yay wiki: https://en.wikipedia.org/wiki/A*_search_algorithm
         public List<Coordinate> FindPath(Coordinate start, Coordinate goal)
         {
-            if (!GetNeighbors(start).Any()) throw new Exception("Starting at a boxed in location!");
+            var queue = new Queue<Coordinate>();
+            queue.Enqueue(start);
 
-            // The set of nodes already evaluated.
-            var closedSet = new HashSet<Coordinate>();
-
-            // The set of currently discovered nodes still to be evaluated.
-            // Initially, only the start node is known.
-            var openSet = new HashSet<Coordinate> { start };
-
-            // For each node, which node it can most efficiently be reached from.
-            // If a node can be reached from many nodes, cameFrom will eventually contain the
-            // most efficient previous step.
+            // They'll also be in cameFrom.Values but the hashSet is faster
+            var found = new HashSet<Coordinate> { start };
             var cameFrom = new Dictionary<Coordinate, Coordinate>();
 
-            // For each node, the cost of getting from the start node to that node.
-            // The cost of going from start to start is zero.
-            var gScore = new Dictionary<Coordinate, int> {{start, 0}};
-
-            // For each node, the total cost of getting from the start node to the goal
-            // by passing by that node. That value is partly known, partly heuristic.
-            // For the first node, that value is completely heuristic.
-            var fScore = new Dictionary<Coordinate, int> {{start, HeuristicCostEstimate(start, goal)}};
-
-            while (openSet.Any())
+            while (queue.Any())
             {
-                // the node in openSet having the lowest fScore[] value
-                var current = openSet.OrderBy(c => fScore.ContainsKey(c) ? fScore[c] : int.MaxValue).First();
+                var current = queue.Dequeue();
+
                 if (current.Equals(goal))
                 {
                     var path = new List<Coordinate> {current};
@@ -58,32 +41,16 @@ namespace AoC2016.Solutions.Day13
                     return path;
                 }
 
-                openSet.Remove(current);
-                closedSet.Add(current);
-
                 foreach (var neighbor in GetNeighbors(current))
                 {
-                    // Ignore the neighbor which is already evaluated.
-                    if (closedSet.Contains(neighbor)) continue;
-
-                    // The distance from start to a neighbor
-                    var tentativeScore = gScore[current] + 1; // 1 is distance between each map point
-                    if (!openSet.Contains(neighbor))
-                    {
-                        openSet.Add(neighbor);
-                    }
-
-                    // This is not a better path.
-                    if (tentativeScore >= (gScore.ContainsKey(neighbor) ? gScore[neighbor] : int.MaxValue)) continue;
-
-                    // This path is the best until now. Record it!
-                    cameFrom[neighbor] = current;
-                    gScore[neighbor] = tentativeScore;
-                    fScore[neighbor] = tentativeScore + HeuristicCostEstimate(neighbor, goal);
+                    if (found.Contains(neighbor)) continue;
+                    found.Add(neighbor);
+                    cameFrom.Add(neighbor, current);
+                    queue.Enqueue(neighbor);
                 }
             }
-
             throw new Exception("No valid path found");
+
         }
 
         public HashSet<Coordinate> FindReachableCoordinates(Coordinate start, int maxDepth)
@@ -125,12 +92,6 @@ namespace AoC2016.Solutions.Day13
                 .Where(_ => _.X >= 0 && _.Y >= 0)
                 .Where(_ => _maze.IsOpen(_.X, _.Y))
                 .ToImmutableHashSet();
-        }
-
-
-        public int HeuristicCostEstimate(Coordinate start, Coordinate goal)
-        {
-            return Math.Abs(start.X - goal.X) + Math.Abs(start.Y - goal.Y);
         }
     }
 }
